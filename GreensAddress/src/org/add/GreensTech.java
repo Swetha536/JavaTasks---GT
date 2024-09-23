44bb599de03c18fe8efd164566338d1430117995
Swetha536 <146423837+Swetha536@users.noreply.github.com>
2024-09-23 16:17:45 +0530

    Add files via upload

---
+++ GreensAddress/src/org/add/GreensTech.java
@@ -0,0 +1,34 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr() {
+		System.out.println("Branch 1");
+
+	}
+	private void greensAdayar() {
+		System.out.println("Branch 2");
+
+	}
+	private void greenstambaram() {
+		System.out.println("Branch 3");
+		
+	}
+	private void greensVelacherry() {
+		System.out.println("Branch 4");
+ 
+
+	}
+	private void greensAnnaNagar() {
+		System.out.println("Branch 5");
+
+	}
+	public static void main(String[] args) {
+		GreensTech G = new GreensTech();
+		G.greensOmr();
+		G.greensAdayar();
+		G.greenstambaram();
+		G.greensVelacherry();
+		G.greensAnnaNagar();
+	}
+
+}
